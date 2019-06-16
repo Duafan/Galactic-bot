5f2905bff966b8b85cd6020473501cc05464ded3
Duafan <duafanisthebest@gmail.com>
2019-06-16 20:56:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ bot.on('guildMemberRemove', member =>{
 bot.on('message', message=>{
 
     let args = message.content.substring(PREFIX.length).split(" ");
-
+    
+    function duration(ms) {
+        const sec = Math.floor((ms / 1000) % 60).toString()
+        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds.`
+    }
+    
     switch (args[0]){
         case 'ping':
             message.channel.send({embed:{ 
@@ -38,6 +46,9 @@ bot.on('message', message=>{
                 description:"Pong!",
                 color: 0x5DADE2   
             }});
+            break;
+        case 'uptime':
+            message.channel.send(`I have been online for: ${duration(bot.uptime)}`)
             break;
         case 'hi':
                 message.reply('Hello!');
