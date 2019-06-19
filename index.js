ecf59e23e8747c9567f72ff6ae762dcffd0c8de2
Duafan <duafanisthebest@gmail.com>
2019-06-19 16:11:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ client.on('guildMemberRemove', member =>{
     channel.send(`Sampai jumpa ${member}! Terima kasih. :upside_down:  `)
 });
 
+ function duration(ms) {
+        const sec = Math.floor((ms / 1000) % 60).toString()
+        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds.`
+    
+    }
+
 //Listener events
 client.on('message', message => {
 
@@ -55,15 +64,6 @@ client.on('message', message => {
 
     } catch (e) {
         console.log(e.stack)
-    }
-    
-        function duration(ms) {
-        const sec = Math.floor((ms / 1000) % 60).toString()
-        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
-        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
-        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds.`
-    
     }
 
     
