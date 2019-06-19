0a0e896afb30fb96723dd356a131e6fbe523fffb
Duafan <duafanisthebest@gmail.com>
2019-06-19 17:11:50 +0700

    Update hapus.js

--- commands/hapus.js
+++ commands/hapus.js
@@ -1,4 +1,12 @@
+const Discord = require('discord.js');
+
 exports.run = async (client, message, args) => {
-    if(!args[1]) return message.reply('Command masih kurang')
-            message.channel.bulkDelete(args[1])
-}
+
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You must specify the number of text to delete');
+    if(!args[0]) return message.channel.send('You must specify the number of messages to delete');
+
+    message.channel.bulkDelete(args[0]).then(() => { 
+        message.channel.send(`:pencil2: ${args[0]} messages have been deleted.`).then(msg => msg.delete(5000));
+    });
+
+} 
