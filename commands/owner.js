dae8586a044c265cf76eaebe60f0087e398574cb
Duafan <duafanisthebest@gmail.com>
2019-06-21 14:17:08 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -12,7 +12,7 @@ exports.run = async (client, message, args) =>{
    message.channel.send(embed).then(message => {
       embed.setColor(0x43f033)
       embed.setDescription("Owned by Galactic")
-      embed.setDescription("Created by zexter")
+      embed.addfield("Created by zexter")
       embed.setFooter(useruser, userurl)
       embed.setTimestamp()
       message.edit(embed)
