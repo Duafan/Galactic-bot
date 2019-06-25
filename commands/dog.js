62e7dce4ff8e4aa45fd943458867d748fe0379a5
Duafan <duafanisthebest@gmail.com>
2019-06-25 18:08:56 +0700

    updated

---
+++ commands/dog.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const PREFIX = "."
+const superagent = require('superagent');
+
+module.exports.command = {
+    name: "dog",
+    aliases: ["anjing"],
+    description: "To shows dog game picture",
+    category: "Fun",
+    usage: ".dog"
+}
+
+// This is the brackets in which the command goes in
+module.exports.run = async (client, message, args) => {
+    if (!message.content.startsWith(PREFIX)) return;
+
+    let msg = await message.channel.send("Generating...")
+
+    let {body} = await superagent
+    .get(`https://dog.ceo/api/breeds/image/random`);
+   // console.log(body.message)
+    if(!{body}) return message.channel.send("I broke! Try again.")
+
+    message.channel.send({embed: {
+        color: 3447003,
+        image: {
+            url: (body.message)
+          },
+          timestamp: new Date(),
+          footer: {
+          icon_url: client.user.displayAvatarURL ,
+          text: "© Galactic Bot",
+        },
+        author: {
+            icon_url: message.guild.iconURL,
+            name: "Galactic Bot",
+          }
+        }});
+
+        msg.delete();
+      } 
+
+
+      
