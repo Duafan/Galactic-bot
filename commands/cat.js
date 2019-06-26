7f48db7bf614d54f83f50d8a46997e34d0bf8a0a
Duafan <duafanisthebest@gmail.com>
2019-06-26 22:29:40 +0700

    updated

---
+++ commands/cat.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const superagent = require("superagent")
+
+module.exports.command = {
+    name: "cat",
+    aliases: ["cat"],
+    description: "Shows random cat images",
+    category: "Fun",
+    usage: ".cat"
+}
+exports.run = (client, message, args) => {
+    let msg = await message.channel.send("Generating...")
+
+    let{body} = await superagent
+    .get(`http://aws.random.cat/meow`)
+    
+    if(!{body}) return message.channel.send("Something went wrong, please try again!")
+
+        let embed = new Discord.RichEmbed()
+        .setColor(0x43f033)
+        .setAuthor(`CATS!`, message.guild.iconURL)
+        .setImage(body.file)
+        .setTimestamp()
+        .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
+
+        msg.delete();
+
+}
---
+++ commands/meme.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const superagent = require("superagent")
+
+module.exports.command = {
+    name: "meme",
+    aliases: ["mim", "memes"],
+    description: "Shows random meme images",
+    category: "Fun",
+    usage: ".meme"
+}
+
+exports.run = (client, message, args) => {
+    let msg = await message.channel.send("Generating...")
+
+    let{body} = await superagent
+    .get(`https://apis.duncte123.me/meme`)
+    
+    if(!{body}) return message.channel.send("Something went wrong, please try again!")
+
+        let embed = new Discord.RichEmbed()
+        .setColor(0x43f033)
+        .setAuthor(`MEMES!`, message.guild.iconURL)
+        .setImage(body.file)
+        .setTimestamp()
+        .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
+
+        msg.delete();
+}
