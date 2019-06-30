885affa3eebf9669f4a7b489165768f912abaca4
Duafan <duafanisthebest@gmail.com>
2019-06-30 11:34:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,22 +35,12 @@ client.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.find(channel => channel.name === "welcome");
     if(!channel) return;
 
-    let user;
-
-    if (message.mentions.users.first()) {
-         user = message.mentions.users.first();
-    } else {
-         user = message.author;
-     }
-
-     const member = message.guild.member(user);
-
     const embedW = new Discord.RichEmbed() // Use Discord.MessageEmbed if you use the master version
     .setColor('#4dfff6') // I just put random in here, but you can chnage it to anything else.
-    .setThumbnail(user.avatarURL)
+    .setThumbnail(member.avatarURL)
     .setTitle(`MEMBER BARU!`)
     .addField(`Selamat datang di Galactic Discord Server, ${member}! Selamat bersenang-senang! :slight_smile: `,  true)
-    .addField(`ID`, `${user.id}`, true)
+    .addField(`ID`, `${member.id}`, true)
     .addField(`Anda member ke`, message.guild.memberCount, true)
     .setTimestamp()
     .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
@@ -62,24 +52,13 @@ client.on('guildMemberRemove', member =>{
    
     const channel = member.guild.channels.find(channel => channel.name === "goodbye");
     if(!channel) return;
-    
-    let user;
-
-    if (message.mentions.users.first()) {
-         user = message.mentions.users.first();
-    } else {
-         user = message.author;
-     }
-
-     const member = message.guild.member(user);
-
 
     const embedB = new Discord.RichEmbed() // Use Discord.MessageEmbed if you use the master version
     .setColor('#4dfff6') // I just put random in here, but you can chnage it to anything else.
-    .setThumbnail(user.avatarURL)
+    .setThumbnail(member.avatarURL)
     .setTitle(`MEMBER KELUAR!`)
-    .addField(`Sampai jumpa ${user.username}#${user.discriminator}! Terima kasih. :upside_down: `,  true)
-    .addField(`ID`, `${user.id}`, true)
+    .addField(`Sampai jumpa ${member.username}#${member.discriminator}! Terima kasih. :upside_down: `,  true)
+    .addField(`ID`, `${member.id}`, true)
     .addField(`Member saat ini`, message.guild.memberCount, true)
     .setTimestamp()
     .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
