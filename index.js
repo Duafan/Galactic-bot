6ebcfb7a96014d44a0c260af184eaac973dd6448
Duafan <duafanisthebest@gmail.com>
2019-06-30 11:11:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,17 @@ client.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.find(channel => channel.name === "welcome");
     if(!channel) return;
 
-    channel.send(`Selamat datang di Galactic Discord Server, ${member}! Selamat bersenang-senang! :slight_smile: `)
+    const embedW = new Discord.RichEmbed() // Use Discord.MessageEmbed if you use the master version
+    .setColor('RANDOM') // I just put random in here, but you can chnage it to anything else.
+    .setThumbnail(user.avatarURL)
+    .setTitle(`MEMBER BARU!`)
+    .addField(`Selamat datang di Galactic Discord Server, ${member}! Selamat bersenang-senang! :slight_smile: `,  true)
+    .addField(`ID`, `${user.id}`, true)
+    .addField(`Anda member ke`, message.guild.memberCount, true)
+    .setTimestamp()
+    .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
+
+    channel.send(embedW)
 });
 
 client.on('guildMemberRemove', member =>{
@@ -43,7 +53,17 @@ client.on('guildMemberRemove', member =>{
     const channel = member.guild.channels.find(channel => channel.name === "goodbye");
     if(!channel) return;
 
-    channel.send(`Sampai jumpa ${member}! Terima kasih. :upside_down:  `)
+    const embedB = new Discord.RichEmbed() // Use Discord.MessageEmbed if you use the master version
+    .setColor('RANDOM') // I just put random in here, but you can chnage it to anything else.
+    .setThumbnail(user.avatarURL)
+    .setTitle(`MEMBER KELUAR!`)
+    .addField(`Sampai jumpa ${user.username}#${user.discriminator}! Terima kasih. :upside_down: `,  true)
+    .addField(`ID`, `${user.id}`, true)
+    .addField(`Member saat ini`, message.guild.memberCount, true)
+    .setTimestamp()
+    .setFooter(`Galactic Bot`, client.user.displayAvatarURL)
+
+    channel.send(embedB)
 });
 
 //Listener events
