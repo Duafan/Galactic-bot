ac4435a213f11d876ba88d274cbb8796218c06a4
Duafan <duafanisthebest@gmail.com>
2020-10-20 19:20:03 +0700

    Create emoji.js

---
+++ commands/emoji.js
@@ -0,0 +1,35 @@
+module.exports.command = {
+    name: "emoji",
+    aliases: ["emojiinfo"],
+    description: "View all emojis in the guild",
+    category: "Util",
+    usage: ".emojify <your text>"
+}
+
+exports.run = async (client, message, args, ) => {
+    let Emojis = "";
+    let EmojisAnimated = "";
+    let EmojiCount = 0;
+    let Animated = 0;
+    let OverallEmojis = 0;
+    function Emoji(id) {
+      return bot.emojis.cache.get(id).toString();
+    }
+    message.guild.emojis.cache.forEach((emoji) => {
+      OverallEmojis++;
+      if (emoji.animated) {
+        Animated++;
+        EmojisAnimated += Emoji(emoji.id);
+      } else {
+        EmojiCount++;
+        Emojis += Emoji(emoji.id);
+      }
+    });
+    let Embed = new MessageEmbed()
+      .setTitle(`Emojis in ${message.guild.name}.`)
+      .setDescription(
+        `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**Over all emojis [${OverallEmojis}]**`
+      )
+      .setColor(`RANDOM`);
+    message.channel.send(Embed);
+};
